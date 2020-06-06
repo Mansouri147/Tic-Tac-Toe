a06f41a86dcd154f7cebf76ad0eebd6abe60e9fa
Mansouri147 <mansouriwhafidoumansouri@gmail.com>
2020-06-06 21:57:31 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,177 @@
+#-----Global Variables----
+
+board = ["-", "-", "-",
+         "-", "-", "-",
+         "-", "-", "-"]
+
+
+game_still_going = True
+winner= None
+current_player = 'X'
+
+
+
+def display_board():
+  print(board[0] + " | "+board[1] + " | " +board[2] )
+  print(board[3] + " | "+board[4] + " | " +board[5] )
+  print(board[6] + " | "+board[7] + " | " +board[8] )
+
+
+
+# Tic Tac Toe game
+def play_game():
+
+
+  display_board()
+
+  while game_still_going:
+    
+    handle_turn(current_player)
+
+    check_if_game_over()
+
+    flip_player()
+
+
+
+  if winner == 'X' or winner == 'O':
+    print(winner + ' won.')
+  elif winner== None:
+    print('Tie.')
+
+
+
+def handle_turn(player):
+
+  print(player + "'s turn.")
+  position = input('Choose a position from 1 to 9: ')
+  
+  valid= False
+  while not valid:
+    while position not in ["1", "2", "3", "4", "5", "6", "7", "8", "9"]:
+      position =input("Please choose a position from 1 to 9: ")
+
+    position = int(position) -1 
+
+    if board[position] == "-":
+      valid= True
+    else:
+      print("You can't go there. Go again.")
+
+
+  board[position] = player
+  display_board()
+
+
+
+def check_if_game_over():
+  check_for_winner()
+  check_if_tie()
+
+
+
+def check_for_winner():
+
+
+  # like this it will refer to the global one if you wanna #access a variable outside the function you must do it
+  global winner
+  
+  row_winner = check_rows()
+  column_winner = check_columns()
+  diagonal_winner = check_diagonals()
+  
+
+  # Get the winner
+  if row_winner:
+    winner = row_winner
+  elif column_winner:
+    winner = column_winner
+  elif diagonal_winner:
+    winner = diagonal_winner
+  else:
+  # there are no winner
+    winner = None
+  return
+
+
+def check_rows():
+
+  global game_still_going
+  # Rows are the same (but not empty != "-")
+  row_1 = board[0] == board[1] == board[2] != "-"
+  row_2 = board[3] == board[4] == board[5] != "-"
+  row_3 = board[6] == board[7] == board[8] != "-"
+  
+  # If there is a match, flag that there is a win
+  if row_1 or row_2 or row_3:
+    game_still_going = False
+  
+  # Return the winner (X or O)
+  if row_1:
+    return board[0]
+  elif row_2:
+    return board[3]
+  elif row_3:
+    return board[6]
+  return
+
+def check_columns():
+
+  global game_still_going
+  # columns are the same (but not empty != "-")
+  column_1 = board[0] == board[3] == board[6] != "-"
+  column_2 = board[1] == board[4] == board[7] != "-"
+  column_3 = board[2] == board[5] == board[8] != "-"
+  
+  # If there is a match, flag that there is a win
+  if column_1 or column_2 or column_3:
+    game_still_going = False
+  
+  # Return the winner (X or O)
+  if column_1:
+    return board[0]
+  elif column_2:
+    return board[1]
+  elif column_3:
+    return board[2]
+  return
+
+def check_diagonals():
+  
+  global game_still_going
+  # diagonals are the same (but not empty != "-")
+  diagonal_1 = board[0] == board[4] == board[8] != "-"
+  diagonal_2 = board[2] == board[4] == board[6] != "-"
+  
+  # If there is a match, flag that there is a win
+  if diagonal_1 or diagonal_2:
+    game_still_going = False
+  
+  # Return the winner (X or O)
+  if diagonal_1:
+    return board[0]
+  elif diagonal_2:
+    return board[2]
+  return
+
+
+
+
+def check_if_tie():
+  global game_still_going
+  if '-' not in board:
+    game_still_going= False
+  return
+
+
+
+def flip_player():
+  global current_player
+  if current_player== "X":
+    current_player = "O"
+  elif current_player == "O":
+    current_player = "X"
+  return
+
+
+play_game()
